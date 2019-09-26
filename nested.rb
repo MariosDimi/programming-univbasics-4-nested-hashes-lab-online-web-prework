b4cda58112f285a71215850cea9cbdbd91ac2ff9
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-26 21:33:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-        programmer_hash[]
+        programmer_hash[:alan_kay][]
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
